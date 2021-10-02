b9955de492fcbce23e0c2806e5218b70528bd452
Mridul-Agarwal <74709416+Mridul-Agarwal@users.noreply.github.com>
2021-10-02 23:51:21 +0530

    Add files via upload

---
+++ PHP Assignment ADAM And Game/Adam.php
@@ -0,0 +1,40 @@
+<?php
+    $num=12;
+
+function reverseDigits($num)
+{
+	$rev = 0;
+	while ($num > 0)
+	{
+		$rev = $rev * 10 + $num % 10;
+		$num = (int) $num / 10;
+	}
+	return $rev;
+}
+
+
+function square($num)
+{
+	return ($num * $num);
+}
+
+function checkAdamNumber($num)
+{
+	
+	$a = square($num);
+	$b = square(reverseDigits($num));
+	
+	
+	if ($a == reverseDigits($b))
+	return 0;
+	return -1;
+}
+
+
+if (checkAdamNumber($num))
+echo "Adam Number";
+else
+echo "Not a Adam Number";
+
+
+?>
